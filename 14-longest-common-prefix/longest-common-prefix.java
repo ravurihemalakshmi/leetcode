03eac393d75e6881df779abe33b294513199d19c
ravurihemalakshmi <24211a05gg@gmail.com>
2026-08-18 11:44:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (32.78%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,8 +1,7 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs==null||strs.length==0) return "";
         String prefix=strs[0];
-        for(int i=0;i<strs.length;i++){
+        for(int i=1;i<strs.length;i++){
             while(!strs[i].startsWith(prefix)){
                 prefix=prefix.substring(0,prefix.length()-1);
             }
@@ -11,6 +10,5 @@ class Solution {
             }
         }
         return prefix;
-        
     }
 }
