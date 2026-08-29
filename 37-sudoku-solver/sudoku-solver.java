8c426e43aca426827b66708d3f9d463a128a09ed
ravurihemalakshmi <24211a05gg@gmail.com>
2026-08-29 11:43:05 +0530

    Time: 398 ms (6.49%) | Memory: 42.9 MB (58.11%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    public boolean solve(char[][] board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(isValid(i,j,ch,board)){
+                            board[i][j]=ch;
+                            if(solve(board)){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+    
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isValid(int row,int col,char ch,char[][] board){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==ch){
+                return false;
+            }
+            if(board[row][i]==ch){
+                return false;
+            }
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==ch){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
